1bd5c7e879cb1a6e323d1282dfc33953b24ca1f3
Cat6172 <65912861+Cat6172@users.noreply.github.com>
2023-03-17 09:02:38 -0600

    Replace config as class attribute with c.getConfig() everywhere, both c and config don't need to exist simultaneously and c is needed to save as well

--- src/main/java/mc/uofa/cat6172/customjoinmessages/MessageStorage.java
+++ src/main/java/mc/uofa/cat6172/customjoinmessages/MessageStorage.java
@@ -3,7 +3,6 @@ package mc.uofa.cat6172.customjoinmessages;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -13,29 +12,28 @@ import static org.bukkit.plugin.java.JavaPlugin.getPlugin;
 public class MessageStorage {
     private static final NamedTextColor messageColor = NamedTextColor.YELLOW;
     private static final CustomJoinMessages c = getPlugin(CustomJoinMessages.class);
-    private static final FileConfiguration config = c.getConfig();
     public static void setJoinMessage(String playerName, String message_raw){
         String message = message_raw.replace("_", " ").replace("\\&", "§");
-        config.set("JoinDB."+playerName, message);
+        c.getConfig().set("JoinDB."+playerName, message);
         c.saveConfig();
     }
     public static TextComponent getJoinMessage(String playerName){
         String fallback = "ERROR: No join message exists for given player: " + playerName;
-        String message = config.getString("JoinDB." + playerName, fallback);
+        String message = c.getConfig().getString("JoinDB." + playerName, fallback);
         return Component.text(message, messageColor);
     }
     public static void removeJoinMessage(String playerName){
-        config.set("JoinDB."+playerName, null);
+        c.getConfig().set("JoinDB."+playerName, null);
         c.saveConfig();
     }
     public static Collection<String> getPlayers(){
         try{
-            return config.getConfigurationSection("JoinDB").getKeys(false);
+            return c.getConfig().getConfigurationSection("JoinDB").getKeys(false);
         } catch (NullPointerException e){
             return Collections.emptySet();
         }
     }
     public static boolean hasJoinMessage(String playerName){
-        return config.getString("JoinDB."+playerName) != null;
+        return c.getConfig().getString("JoinDB."+playerName) != null;
     }
 }
