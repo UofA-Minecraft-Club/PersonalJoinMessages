57e4a92bb25648e35a1e4975566844474ae5546a
Ilya-Nalivaiko <65912861+Ilya-Nalivaiko@users.noreply.github.com>
2025-02-14 00:32:15 -0700

    dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
